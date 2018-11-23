291e6d46e7321de6aa843eec7be8a48ba33fa0a9
mmnr00 <mmnr00@gmail.com>
2018-11-23 23:55:50 +0800

    asset

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module WebarchAll
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.assets.paths << Rails.root.join("vendor")
+    config.assets.paths << Rails.root.join("vendor","assets")
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
